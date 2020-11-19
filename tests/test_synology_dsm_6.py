a2a2d4c5639fc1feb185546a62b17d8afdc1b9a3
mib1185 <mail@mib85.de>
2020-11-19 22:54:36 +0000

    add hyper backup

--- tests/test_synology_dsm_6.py
+++ tests/test_synology_dsm_6.py
@@ -470,3 +470,14 @@ class TestSynologyDSM6:
         assert dsm_6.surveillance_station.get_home_mode_status()
         assert dsm_6.surveillance_station.set_home_mode(False)
         assert dsm_6.surveillance_station.set_home_mode(True)
+
+    def test_hyper_backup(self, dsm_6):
+        """Test HyperBackup."""
+        assert dsm_6.api.backup
+        assert not dsm_6.api.backup.get_all_tasks()
+
+        dsm_6.api.backup.update()
+        assert dsm_6.api.backup.get_all_tasks()
+        assert dsm_6.api.backup.get_task(1)
+        assert dsm_6.api.backup.get_task(2)
+        assert dsm_6.api.backup.get_task(3)
