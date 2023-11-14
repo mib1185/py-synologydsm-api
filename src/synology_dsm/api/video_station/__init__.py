2d25c6d9fe4cbcc04509f8d5c8f59d5de8d260b9
BoutXIII <bouthier.valentin@gmail.com>
2023-11-14 17:33:25 +0100

    add function VideoStation

--- src/synology_dsm/api/video_station/__init__.py
+++ src/synology_dsm/api/video_station/__init__.py
@@ -5,7 +5,7 @@ import json
 
 from synology_dsm.api import SynoBaseApi
 
-from .video import SynoVideoStationMovie, SynoVideoStationMoviePoster
+from .video import SynoVideoStationMovie, SynoVideoStationMoviePoster, SynoVideoStationDevices, SynoVideoStationMetadataMovie
 
 
 class SynoVideoStation(SynoBaseApi):
@@ -16,17 +16,18 @@ class SynoVideoStation(SynoBaseApi):
     POSTER_API_KEY = "SYNO.VideoStation2.Poster"
     TVSHOW_API_KEY = "SYNO.VideoStation2.TVShow"
     TVSHOW_EPISODE_API_KEY = "SYNO.VideoStation2.TVShowEpisode"
+    CONTROLLER_DEVICE_API_KEY = "SYNO.VideoStation2.Controller.Device"
 
-    async def get_movies(
+    async def get_list_movie(
         self, offset: int = 0, limit: int = 100, sort_by: bool = True, sort_direction: str = 'desc', library_id: int = 0
     ) -> list[SynoVideoStationMovie] | None:
         """Get a list of all movies."""
-        movies: list[SynoVideoStationMovie] = []
         if bool(sort_by):
             sort_by = 'added'
         else:
             sort_by = ''
             sort_direction = 'asc'
+
         raw_data = await self._dsm.get(
             self.MOVIE_API_KEY, "list", 
             {
@@ -35,28 +36,36 @@ class SynoVideoStation(SynoBaseApi):
                 "sort_by": sort_by,
                 "sort_direction": sort_direction,
                 "library_id": library_id,
-                "additional": '["poster_mtime","summary","watched_ratio","extra","file"]'
+                "additional": '["poster_mtime","summary","watched_ratio","extra","collection"]'
             }
         )
+        return self._raw_data_to_movies(raw_data)
+    
+    def _raw_data_to_movies(
+        self, raw_data: bytes | dict | str
+    ) -> list[SynoVideoStationMovie] | None:
+        """Parse the raw data response to a list of photo items."""
+        movies: list[SynoVideoStationMovie] = []
         if not isinstance(raw_data, dict) or (data := raw_data.get("data")) is None:
             return None
+
         for movie in data["movie"]:
             poster_str = movie["additional"]["extra"]
             poster = json.loads(poster_str)
+
             movies.append(
                 SynoVideoStationMovie(
                     movie["id"],
                     movie["title"],
-                    movie["additional"]["summary"],
-                    poster["com.synology.TheMovieDb"]["poster"]
+                    movie["additional"]["summary"]
+                    # poster["com.synology.TheMovieDb"]["poster"]
                 )
             )
         return movies
 
-    async def get_poster_from_movie(
-        self, movie: SynoVideoStationMovie
-    ) -> list[SynoVideoStationMoviePoster] | None:
-        """Get a poster from movie."""
+    async def get_url_poster(
+        self, movie: SynoVideoStationMovie) -> bytes | None:
+        """Get a poster from movie or TV Show."""
         raw_data = await self._dsm.get(
             self.POSTER_API_KEY,
             "get",
@@ -69,3 +78,27 @@ class SynoVideoStation(SynoBaseApi):
         if isinstance(raw_data, bytes):
             return raw_data
         return None
+    
+    async def get_devices(self)-> list[SynoVideoStationDevices] | None:
+        """Get a Devices."""
+        raw_data = await self._dsm.get(
+            self.CONTROLLER_DEVICE_API_KEY,
+            "list",
+            {
+                "limit": 50000
+            },
+        )
+        return raw_data
+    
+    async def get_metadata_movie(
+        self, movie: SynoVideoStationMovie)-> list[SynoVideoStationMetadataMovie] | None:
+        """Get a metadata in movie."""
+        raw_data = await self._dsm.get(
+            self.MOVIE_API_KEY,
+            "getinfo",
+            {
+                "id": movie.movie_id,
+                "additional": '["summary","poster_mtime","backdrop_mtime","file","collection","watched_ratio","conversion_produced","actor","director","genre","writer","extra"]'
+            },
+        )
+        return raw_data
--- src/synology_dsm/api/video_station/video.py
+++ src/synology_dsm/api/video_station/video.py
@@ -12,7 +12,23 @@ class SynoVideoStationMovie:
 
 @dataclass
 class SynoVideoStationMoviePoster:
-    """Representation of an Synology VideoStation Movies."""
+    """Representation of an Synology VideoStation Poster."""
 
     movie_id: int
     movie_type: str
+
+@dataclass
+class SynoVideoStationDevices:
+    """Representation of an Synology VideoStation Devices."""
+
+    device_id: str
+    device_now_playing: str
+    password_protected: bool
+    device_title: str
+
+@dataclass
+class SynoVideoStationMetadataMovie:
+    """Representation of an Synology VideoStation Metadata."""
+
+    file_id: int
+    certificate: str
